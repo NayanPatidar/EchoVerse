1f0dd7536ddc6405b2d8f905809de4f632478cc0
Nayan Patidar <nayanpatidar28@gmail.com>
2024-07-31 18:28:33 +0530

    Sidebar Category Selection

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,9 +4,11 @@ const Navbar = () => {
   return (
     <div className=" absolute flex bg-black w-full h-16 z-100 top-0 border-b-[1px] border-b-zinc-800">
       <div className=" w-[15rem] flex gap-3 h-16 items-center text-center justify-center">
-        <PiListLight size={24} color="white" />
+        <div className=" w-10 h-10 flex items-center justify-center hover:bg-zinc-800 rounded-full cursor-pointer">
+          <PiListLight size={24} color="white" />
+        </div>
         <span>
-          <img src="./LogoEchoVerse.png" className=" w-40" />
+          <img src="./LogoEchoVerse2.png" className=" w-40" />
         </span>
       </div>
     </div>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,34 +3,61 @@ import { PiListLight } from "react-icons/pi";
 import { MdHomeFilled } from "react-icons/md";
 import { MdOutlineLibraryMusic } from "react-icons/md";
 import { BsChatLeftDots } from "react-icons/bs";
+import { MouseEvent, useEffect, useState } from "react";
 
 const Sidebar = () => {
+  const [musicCategory, setMusicCategory] = useState<number | null>(1);
+
+  const setCategory = (event: React.MouseEvent<HTMLElement>) => {
+    const id = (event.target as HTMLElement).id;
+    setMusicCategory(parseInt(id));
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div className=" bg-black w-[15rem] h-screen text-white border-r-[1px] border-zinc-800">
       <div className=" flex gap-6 h-16 items-center text-center justify-center">
-        <PiListLight size={24} />
+        <div>
+          <PiListLight size={24} />
+        </div>
         <span>
-          <img src="./LogoVerse.png" className=" w-36" />
+          <img src="./LogoEchoVerse2.png" className=" w-36" />
         </span>
       </div>
       <div className=" p-2">
         <ul>
-          <li className="px-5 flex items-center hover:bg-zinc-700 rounded-md">
-            <div className=" h-12 flex items-center gap-5 ">
+          <li
+            className="px-5 flex items-center hover:bg-[#49494988] rounded-md"
+            style={{ backgroundColor: musicCategory == 1 ? "#49494965" : "" }}
+            id="1"
+            onClick={setCategory}
+          >
+            <div className=" h-12 flex items-center gap-5 cursor-pointer">
               <MdHomeFilled size={24} color="white" />
-              <div className=" text-md lato-regular">Home</div>
+              <div className=" text-lg lato-regular">Home</div>
             </div>
           </li>
-          <li className="px-5 flex items-center hover:bg-zinc-700 rounded-md">
-            <div className=" h-12 flex items-center gap-5">
+          <li
+            className="px-5 flex items-center hover:bg-[#49494988] rounded-md"
+            style={{ backgroundColor: musicCategory == 2 ? "#49494965" : "" }}
+            id="2"
+            onClick={setCategory}
+          >
+            <div className=" h-12 flex items-center gap-5 cursor-pointer">
               <MdOutlineLibraryMusic size={24} />
-              <div className=" text-md lato-regular">Library</div>
+              <div className=" text-lg lato-regular">Library</div>
             </div>
           </li>
-          <li className="px-5 flex items-center hover:bg-zinc-700 rounded-md">
-            <div className=" h-12 flex items-center gap-5">
+          <li
+            className="px-5 flex items-center hover:bg-[#49494988] rounded-md"
+            style={{ backgroundColor: musicCategory == 3 ? "#49494965" : "" }}
+            id="3"
+            onClick={setCategory}
+          >
+            <div className=" h-12 flex items-center gap-5 cursor-pointer">
               <BsChatLeftDots size={20} color="white" />
-              <div className=" text-md lato-regular">Chat</div>
+              <div className=" text-lg lato-regular">Chat</div>
             </div>
           </li>
         </ul>
