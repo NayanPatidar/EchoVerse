3695c3749c57a3c9c65d59d3d25612354ba3f4a6
Nayan Patidar <nayanpatidar28@gmail.com>
2024-08-17 21:12:56 +0530

    List Audio Files

--- src/components/listaudiofiles.tsx
+++ src/components/listaudiofiles.tsx
@@ -7,6 +7,7 @@ import { FaPlay } from "react-icons/fa6";
 interface AudioFiles {
   SongsData?: (Song | Episode)[];
   isPlaylist: boolean;
+  isArtist: boolean;
 }
 
 function secondsToTime(seconds: number) {
@@ -15,13 +16,20 @@ function secondsToTime(seconds: number) {
   return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 }
 
-const ListAudioFiles: React.FC<AudioFiles> = ({ SongsData, isPlaylist }) => {
+const ListAudioFiles: React.FC<AudioFiles> = ({
+  SongsData,
+  isPlaylist,
+  isArtist,
+}) => {
+  if (!SongsData) {
+    return;
+  }
   return (
     <div className=" flex flex-col gap-2">
       {SongsData?.map((song, index) => {
         return (
           <div
-            className=" ListAudioFiles flex text-[#d4d4d894] text-sm Montserrat-regular items-center px-5 cursor-pointer transform transition-transform duration-300 hover:bg-[#3636362a] hover:text-[#d4d4d8] rounded-sm h-12"
+            className=" ListAudioFiles flex text-[#d4d4d894] text-sm Montserrat-regular items-center px-5 cursor-pointer transform transition-transform duration-300 hover:bg-[#3636362a] hover:text-[#d4d4d8c5] rounded-sm h-12"
             key={index}
           >
             <span className=" w-6/12 flex gap-4 items-center">
@@ -42,11 +50,26 @@ const ListAudioFiles: React.FC<AudioFiles> = ({ SongsData, isPlaylist }) => {
               ) : (
                 ""
               )}
-              <span className=" text-[#d4d4d8]">{song.name} </span>
+              <span className=" w-8/12 text-[#d4d4d8]  overflow-hidden whitespace-nowrap text-ellipsis">
+                {song.name}
+              </span>
             </span>
-            <span className=" w-2/12 justify-self-end flex justify-end">
-              {song.play_count}
-            </span>
+            {isArtist ? (
+              <span className=" w-3/12 justify-self-start flex justify-start whitespace-nowrap">
+                <span>
+                  <span className=" hover:text-white">
+                    {song.artist_map.artists[0].name},{" "}
+                  </span>
+                  <span className=" hover:text-white">
+                    {song.artist_map.artists[1].name}
+                  </span>
+                </span>
+              </span>
+            ) : (
+              <span className=" w-2/12 justify-self-end flex justify-end">
+                {song.play_count}
+              </span>
+            )}
             <span className=" w-4/12 justify-self-end pb-0.5 flex justify-end">
               {secondsToTime(song.duration)}
             </span>
