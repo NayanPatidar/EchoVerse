dc895fccb7225495fe827294b644309fde1fe9c1
Nayan Patidar <nayanpatidar28@gmail.com>
2024-08-19 06:47:11 +0530

    CardSkeleton Update

--- src/components/skeletons.tsx
+++ src/components/skeletons.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Skeleton from "@mui/material/Skeleton";
+import { Box } from "@mui/material";
 
 export function CardSkeleton() {
   return (
     <div className="media-element flex flex-col text-left">
-      <Skeleton sx={{ bgcolor: "grey.800" }} height={160} />
+      <Box
+        sx={{
+          width: 160,
+          aspectRatio: "1 / 1",
+        }}
+        style={{
+          borderRadius: "50%", // Fully rounded (circle)
+        }}
+      >
+        <Skeleton
+          sx={{
+            bgcolor: "grey.800",
+            borderRadius: "4px",
+          }}
+          variant="rectangular"
+          width="100%"
+          height="100%"
+        />
+      </Box>
       <span className=" text-base lato-regular mt-5  overflow-hidden whitespace-nowrap text-ellipsis">
         <Skeleton sx={{ bgcolor: "grey.800" }} />
       </span>
