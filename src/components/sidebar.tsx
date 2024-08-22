3544acb88b71ce21e0dfea3825f13fd4c44544be
Nayan Patidar <nayanpatidar28@gmail.com>
2024-08-22 16:26:34 +0530

    Sidebar Top Artist, Albums,  Songs Added

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,10 +2,13 @@
 import { useRouter } from "next/navigation";
 import { PiListLight } from "react-icons/pi";
 import { MdHome } from "react-icons/md";
-import { MdOutlineLibraryMusic } from "react-icons/md";
 import { BsFillChatLeftTextFill } from "react-icons/bs";
 import { AiOutlineLike } from "react-icons/ai";
 import { useEffect, useState } from "react";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import AlbumIcon from "@mui/icons-material/Album";
+import { FaGuitar } from "react-icons/fa";
 import Image from "next/image";
 import {
   Accordion,
@@ -50,58 +53,41 @@ const Sidebar = () => {
         )}
       </div>
       <div className=" p-2 w-full">
-        <ul className=" flex flex-col">
-          <li>
-            <div className=" w-full">
-              <Accordion
-                type="single"
-                value={musicCategory === 1 ? "Library" : ""}
-                collapsible
-              >
-                <AccordionItem
-                  value={"Library"}
-                  className=" flex flex-col gap-1"
-                >
-                  <AccordionTrigger
-                    className="h-12 flex items-center hover:bg-[#49494988] rounded-md "
-                    style={{
-                      backgroundColor: musicCategory === 1 ? "#49494965" : "",
-                      width: sideBarOpen ? "14rem" : "4rem",
-                      justifyContent: sideBarOpen ? "" : "center",
-                      paddingLeft: sideBarOpen ? "22px" : "",
-                      paddingRight: sideBarOpen ? "20px" : "",
-                    }}
-                    onClick={() => setCategory(1)}
-                  >
-                    <div className="h-12 flex items-center gap-4 cursor-pointer">
-                      <MdOutlineLibraryMusic size={24} color="#d6d6d6dc" />
-                      {sideBarOpen ? (
-                        <div className="text-regular lato-regular text-white">
-                          Library
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <div
-                      className="h-12 rounded-md text-sm text-white bg-[#3939396f] flex flex-row items-center"
-                      style={{
-                        width: sideBarOpen ? "14rem" : "3rem",
-                        justifyContent: sideBarOpen ? "" : "center",
-                        paddingLeft: sideBarOpen ? "22px" : "",
-                        paddingRight: sideBarOpen ? "20px" : "",
-                      }}
-                    >
-                      <div className="flex items-center gap-4 cursor-pointer text-regular">
-                        <AiOutlineLike size={24} />
-                        {sideBarOpen ? <span>Liked Songs</span> : ""}
-                      </div>
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
+        <ul className=" flex flex-col gap-1">
+          <li
+            className="px-5 flex items-center hover:bg-[#49494988] rounded-md"
+            style={{
+              backgroundColor: musicCategory == 1 ? "#49494965" : "",
+              justifyContent: sideBarOpen ? "" : "center",
+            }}
+            id="1"
+            onClick={() => setCategory(1)}
+          >
+            <div className=" h-12 flex items-center gap-4 cursor-pointer">
+              <MusicNoteIcon className=" rotate-12" />
+              {sideBarOpen ? (
+                <span className=" text-md lato-regular">Top Songs</span>
+              ) : (
+                ""
+              )}
+            </div>
+          </li>
+          <li
+            className="px-5 flex items-center hover:bg-[#49494988] rounded-md"
+            style={{
+              backgroundColor: musicCategory == 2 ? "#49494965" : "",
+              justifyContent: sideBarOpen ? "" : "center",
+            }}
+            id="2"
+            onClick={() => setCategory(2)}
+          >
+            <div className=" h-12 flex items-center gap-4 cursor-pointer">
+              <AlbumIcon className=" rotate-12" />
+              {sideBarOpen ? (
+                <span className=" text-md lato-regular">Top Albums</span>
+              ) : (
+                ""
+              )}
             </div>
           </li>
           <li
@@ -113,8 +99,87 @@ const Sidebar = () => {
             id="3"
             onClick={() => setCategory(3)}
           >
-            <div className=" h-12 flex items-center gap-4 cursor-pointer pl-1">
-              <BsFillChatLeftTextFill size={20} color="#d6d6d6dc" />
+            <div className=" h-12 flex items-center gap-4 cursor-pointer">
+              <div className=" w-[23px]">
+                <FaGuitar size={20} color="#d6d6d6dc" />
+              </div>
+              {sideBarOpen ? (
+                <span className=" text-md lato-regular">Top Artists</span>
+              ) : (
+                ""
+              )}
+            </div>
+          </li>
+          <li>
+            <div className=" w-full">
+              <Accordion
+                type="single"
+                value={musicCategory === 4 ? "Library" : ""}
+                collapsible
+              >
+                <AccordionItem
+                  value={"Library"}
+                  className=" flex flex-col gap-1"
+                >
+                  <AccordionTrigger
+                    className="h-12 flex items-center hover:bg-[#49494988] rounded-md "
+                    style={{
+                      backgroundColor: musicCategory === 4 ? "#49494965" : "",
+                      width: sideBarOpen ? "14rem" : "4rem",
+                      justifyContent: sideBarOpen ? "" : "center",
+                      paddingLeft: sideBarOpen ? "18px" : "",
+                      paddingRight: sideBarOpen ? "20px" : "",
+                    }}
+                    onClick={() => setCategory(4)}
+                  >
+                    <div className="h-12 flex items-center gap-4 cursor-pointer">
+                      <div className=" w-[25px]">
+                        <LibraryMusicIcon />
+                      </div>
+                      {sideBarOpen ? (
+                        <div className="text-regular lato-regular text-white">
+                          Library
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div
+                      className="h-12 rounded-md text-sm text-white bg-[#3939396f] flex flex-row items-center "
+                      style={{
+                        width: sideBarOpen ? "14rem" : "3rem",
+                        justifyContent: sideBarOpen ? "" : "center",
+                        paddingLeft: sideBarOpen ? "22px" : "",
+                        paddingRight: sideBarOpen ? "20px" : "",
+                      }}
+                    >
+                      <div className="flex items-center gap-4 cursor-pointer text-regular">
+                        <div className=" w-[25px]">
+                          <AiOutlineLike size={24} />
+                        </div>
+                        {sideBarOpen ? <span>Liked Songs</span> : ""}
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          </li>
+          <li
+            className="px-5 flex items-center hover:bg-[#49494988] rounded-md"
+            style={{
+              backgroundColor: musicCategory == 5 ? "#49494965" : "",
+              justifyContent: sideBarOpen ? "" : "center",
+            }}
+            id="5"
+            onClick={() => setCategory(5)}
+          >
+            <div className=" h-12 flex items-center gap-4 cursor-pointer">
+              <div className=" w-[25px]">
+                <BsFillChatLeftTextFill size={20} color="#d6d6d6dc" />
+              </div>
               {sideBarOpen ? (
                 <span className=" text-md lato-regular">Chat</span>
               ) : (
