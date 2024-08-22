410dfedbe235a7fb994036848f248e044f50b914
Nayan Patidar <nayanpatidar28@gmail.com>
2024-08-22 18:39:07 +0530

    Top Artist Page

---
+++ src/app/(root)/(music)/artist/page.tsx
@@ -0,0 +1,28 @@
+import TopArtistCard from "@/components/ui/topArtistCard";
+import { getTopArtists } from "@/lib/api_jiosaavn";
+
+const TopPlaylist = async () => {
+  const playlistData = await getTopArtists();
+  console.log(playlistData);
+
+  return (
+    <div className="TopArtistPage grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4">
+      {playlistData?.map(
+        ({ id, name, image, url, follower_count, is_followed }) => {
+          return (
+            <TopArtistCard
+              id={id}
+              name={name}
+              image={image}
+              url={url}
+              follower_count={follower_count}
+              is_followed={is_followed}
+            />
+          );
+        }
+      )}
+    </div>
+  );
+};
+
+export default TopPlaylist;
