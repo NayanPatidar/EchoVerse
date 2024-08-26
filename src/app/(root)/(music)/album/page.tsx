3856a503cecf47697cb3b2d11380343c6ebc10ea
Nayan Patidar <nayanpatidar28@gmail.com>
2024-08-26 13:32:03 +0530

    Album Page

---
+++ src/app/(root)/(music)/album/page.tsx
@@ -0,0 +1,29 @@
+import TopPlaylistCard from "@/components/ui/Cards/playlistCard";
+import TopArtistCard from "@/components/ui/Cards/topArtistCard";
+import { getTopAlbums, getTopArtists } from "@/lib/api_jiosaavn";
+
+const TopAlbums = async () => {
+  const albumData = await getTopAlbums();
+  console.log(albumData);
+
+  return (
+    <div className="TopArtistPage grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4">
+      {albumData?.data?.map(({ id, name, image, url }, key) => {
+        return (
+          <div key={key}>
+            <TopPlaylistCard
+              key={key}
+              id={id}
+              name={name}
+              image={image}
+              url={url}
+              type="artist"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TopAlbums;
