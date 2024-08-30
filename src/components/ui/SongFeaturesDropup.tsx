0cc2a01cd97a6703d663b080786a91729ad4f055
Nayan Patidar <nayanpatidar28@gmail.com>
2024-08-30 20:11:57 +0530

    Dropup Change

--- src/components/ui/SongFeaturesDropup.tsx
+++ src/components/ui/SongFeaturesDropup.tsx
@@ -1,19 +1,10 @@
 import {
-  Cloud,
-  CreditCard,
-  Github,
   Keyboard,
-  LifeBuoy,
-  LogOut,
-  Mail,
-  MessageSquare,
   MoreVerticalIcon,
-  Plus,
-  PlusCircle,
-  Settings,
-  User,
-  UserPlus,
-  Users,
+  Heart,
+  ListPlus,
+  Rows3,
+  LetterText,
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -22,16 +13,8 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MdAccountCircle } from "react-icons/md";
 
 export function DropupMenuAudioPlayer() {
   return (
@@ -42,19 +25,19 @@ export function DropupMenuAudioPlayer() {
       <DropdownMenuContent className=" w-48 mb-8 mr-5 bg-[#1d1d1d] text-white border-[#333333]">
         <DropdownMenuGroup>
           <DropdownMenuItem>
-            <User className="mr-2 h-4 w-4" />
+            <Heart className="mr-2 h-4 w-4" />
             <span>Add to Favorite</span>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            <CreditCard className="mr-2 h-4 w-4" />
+            <ListPlus className="mr-2 h-4 w-4" />
             <span>Add to Playlist</span>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            <Settings className="mr-2 h-4 w-4" />
+            <Rows3 className="mr-2 h-4 w-4" />
             <span>Add to Queue</span>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            <Keyboard className="mr-2 h-4 w-4" />
+            <LetterText className="mr-2 h-4 w-4" />
             <span>Lyrics</span>
           </DropdownMenuItem>
         </DropdownMenuGroup>
