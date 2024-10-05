60ca6bdaf646bd325aa79d06eec2fe8a75756705
Nayan Patidar <nayanpatidar28@gmail.com>
2024-10-05 10:51:48 +0530

    Request Delete on Accepted and Status Update

--- src/app/(root)/(share)/user/[id]/page.tsx
+++ src/app/(root)/(share)/user/[id]/page.tsx
@@ -10,9 +10,7 @@ import { useEffect, useState } from "react";
 
 const User = ({ params }: { params: { id: string } }) => {
   const [status, setStatus] = useState("FOLLOW"); // "FOLLOW", "FOLLOW_REQUEST_SENT", "FOLLOW_BACK", "FRIENDS"
-  const [followRequests, setFollowRequests] = useState([]);
   const [unsubscribe, setUnsubscribe] = useState<null | (() => void)>(null);
-  const [isFriendsChecked, setIsFriendsChecked] = useState(false);
   const [posts, setPosts] = useState(null);
   const { token, tokenDetails } = useAuthProvider();
   const [AllUser, setAllUser] = useState<CompleteUserData | undefined>(
@@ -30,7 +28,7 @@ const User = ({ params }: { params: { id: string } }) => {
 
   const AddFriend = async () => {
     try {
-      const response = await fetch("/api/friends/addFriend", {
+      const response = await fetch("/api/friends/search&addFriend", {
         method: "POST",
         headers: {
           "content-type": "application/json",
@@ -43,14 +41,15 @@ const User = ({ params }: { params: { id: string } }) => {
       });
 
       const result = await response.json();
+      if (result) {
+        console.log("Friend added to the database");
+      }
     } catch (error: any) {
       console.error("Error is sending friends Request : ", error.message);
     }
   };
 
   const listenForFriendRequests = () => {
-    console.log("It is called : " + tokenDetails.userId);
-
     const friendRequestRef = ref(
       database,
       `friendRequest/${tokenDetails.userId}`
@@ -61,17 +60,21 @@ const User = ({ params }: { params: { id: string } }) => {
 
       if (data) {
         const senderId = params.id;
-        if (data[senderId] && data[senderId].status === "PENDING") {
+        if (
+          data[senderId] &&
+          data[senderId].status === "PENDING" &&
+          data[senderId].type == "SENDER"
+        ) {
           setStatus("FOLLOW_BACK");
-        } else if (data[senderId] && data[senderId].status === "ACCEPTED") {
+        } else if (
+          data[senderId] &&
+          data[senderId].status === "PENDING" &&
+          data[senderId].type == "RECEIVER"
+        ) {
+          setStatus("FOLLOW_REQUEST_SENT");
+        }
+        if (data[senderId] && data[senderId].status === "ACCEPTED") {
           setStatus("FRIENDS");
-          remove(friendRequestRef)
-            .then(() => {
-              console.log("Request removed successfully");
-            })
-            .catch((error) => {
-              console.error("Error removing request:", error);
-            });
         }
       }
     });
@@ -82,7 +85,7 @@ const User = ({ params }: { params: { id: string } }) => {
   const checkFriendStatus = async () => {
     try {
       const response = await fetch(
-        `/api/friends/addFriend?userId=${tokenDetails.userId}&friendID=${params.id}`,
+        `/api/friends/search&addFriend?userId=${tokenDetails.userId}&friendID=${params.id}`,
         {
           method: "GET",
           headers: {
@@ -99,7 +102,6 @@ const User = ({ params }: { params: { id: string } }) => {
       } else {
         listenForFriendRequests();
       }
-      setIsFriendsChecked(true);
     } catch (error: any) {
       console.error("Error checking friend status:", error.message);
     }
@@ -118,15 +120,27 @@ const User = ({ params }: { params: { id: string } }) => {
   }, [tokenDetails]);
 
   const sendFollowRequest = () => {
-    const friendRequestRef = ref(
+    const sendersRequestRef = ref(
       database,
       `friendRequest/${params.id}/${tokenDetails.userId}`
     );
 
-    set(friendRequestRef, {
+    const recieversRequestRef = ref(
+      database,
+      `friendRequest/${tokenDetails.userId}/${params.id}`
+    );
+
+    set(sendersRequestRef, {
       status: "PENDING",
       timeStamp: Date.now(),
       name: AllUser?.name,
+      type: "SENDER",
+    });
+
+    set(recieversRequestRef, {
+      status: "PENDING",
+      timeStamp: Date.now(),
+      type: "RECEIVER",
     });
 
     setStatus("FOLLOW_REQUEST_SENT");
@@ -138,18 +152,32 @@ const User = ({ params }: { params: { id: string } }) => {
       `friendRequest/${params.id}/${tokenDetails.userId}`
     );
 
+    const receiverFriendRequestRef = ref(
+      database,
+      `friendRequest/${tokenDetails.userId}/${params.id}`
+    );
+
     set(friendRequestRef, {
       status: "ACCEPTED",
       timeStamp: Date.now(),
     });
 
     AddFriend();
+
     remove(friendRequestRef)
       .then(() => {
-        console.log("Request removed successfully");
+        console.log("Request removed successfully from sender's side");
       })
       .catch((error) => {
-        console.error("Error removing request:", error);
+        console.error("Error removing request from sender's side:", error);
+      });
+
+    remove(receiverFriendRequestRef)
+      .then(() => {
+        console.log("Request removed successfully from receiver's side");
+      })
+      .catch((error) => {
+        console.error("Error removing request from receiver's side:", error);
       });
 
     setStatus("FRIENDS");
